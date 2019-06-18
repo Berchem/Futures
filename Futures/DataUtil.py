f5fbf253ae798ce22b4a83c5b852777daf33ae3c
Berchem <low118profile@gmail.com>
2019-06-18 23:51:38 +0800

    [UPDATE] Futures: DataUtil: rename

---
+++ Futures/DataUtil.py
@@ -0,0 +1,15 @@
+from MypseudoSQL import Table
+from SQLiteUtil import SQLiteUtil
+
+
+class GetDataUtil:
+    x = 1
+
+    def __init__(self):
+        pass
+
+    def get_data_from_file(self, filename=None, folder=None):
+        pass
+
+    def get_data_from_url(self, url=None):
+        pass
