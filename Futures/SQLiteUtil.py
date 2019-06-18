895c3d957577a05502ffc729069c176360e8cadc
Berchem <34000467+Berchem@users.noreply.github.com>
2019-06-18 19:42:49 +0800

    [UPDATE] Futures: SQLiteTest: pilot class

--- Futures/SQLiteUtil.py
+++ Futures/SQLiteUtil.py
@@ -1,3 +1,47 @@
+import sqlite3
+
+
 class SQLiteUtil:
-    def __init__(self):
-        pass
+    def __init__(self, database):
+        self.__database = database
+        self.conn = self.__get_connection()
+
+    def __get_connection(self):
+        return sqlite3.connect(self.__database)
+
+    def _get(self, table, **kwargs):
+        query = "select * from %s" % table
+
+        if any(filter(lambda k: k in ("start", "drop_night_trade", "predicate"), kwargs)):
+            query += " where"
+            subquery = []
+
+            if 'start' in kwargs.keys():
+                start = kwargs['start']
+                subquery += ["(Date>='%s')" % start]
+
+            if "drop_night_trade" in kwargs.keys():
+                if kwargs["drop_night_trade"]:
+                    subquery += ["(Time>='08:45:00') & (Time<='13:45:00')"]
+
+            query += " " + " & ".join(subquery)
+
+        if "predicate" in kwargs.keys():
+            predicate = kwargs["predicate"]
+            query += " " + predicate
+
+        if "lim" in kwargs.keys():
+            lim = kwargs['lim']
+            query += " limit %d" % lim
+
+        cursor = self.conn.execute(query)
+        return cursor.fetchall()
+
+    def get_each(self, table='tick_log', **kwargs):
+        return self._get(table, **kwargs)
+
+    def get_per_min(self, table='tick_min_log', **kwargs):
+        return self._get(table, **kwargs)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.conn.close()
