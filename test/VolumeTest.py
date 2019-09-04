d70b07603a6e6d4dfc87616ec56aebf71bfff13c
Berchem <low118profile@gmail.com>
2019-09-05 02:30:17 +0800

    [UPDATE] HModel: FuturesPrice: target

--- test/VolumeTest.py
+++ test/VolumeTest.py
@@ -5,23 +5,39 @@ import matplotlib.pyplot as plt
 from Futures.Config import Config
 from HModel.Volume import *
 
-leverage_matrix = [
-    dict(zip(["LEVERAGE", "YearlyROI", "FuturesPrice"], [1.0, 0.113329, 0.07388])),
-    dict(zip(["LEVERAGE", "YearlyROI", "FuturesPrice"], [1.5, 0.176131, 0.100811])),
-    dict(zip(["LEVERAGE", "YearlyROI", "FuturesPrice"], [2.0, 0.196055, 0.105226])),
-    dict(zip(["LEVERAGE", "YearlyROI", "FuturesPrice"], [2.5, 0.234837, 0.092933])),
-    dict(zip(["LEVERAGE", "YearlyROI", "FuturesPrice"], [3.0, 0.234157, 0.066772])),
-    dict(zip(["LEVERAGE", "YearlyROI", "FuturesPrice"], [3.5, 0.242749, 0.028326])),
-    dict(zip(["LEVERAGE", "YearlyROI", "FuturesPrice"], [4.0, 0.219047, -0.08158])),
-    dict(zip(["LEVERAGE", "YearlyROI", "FuturesPrice"], [4.5, 0.195386, -0.09382])),
-    dict(zip(["LEVERAGE", "YearlyROI", "FuturesPrice"], [5.0, 0.151051, -0.11831])),
-    dict(zip(["LEVERAGE", "YearlyROI", "FuturesPrice"], [5.5, 0.095163, -0.12339])),
-    dict(zip(["LEVERAGE", "YearlyROI", "FuturesPrice"], [6.0, 0.016063, -0.13232])),
-]
+sample_matrix = {
+    "LEVERAGE": [
+        dict(zip(["LEVERAGE", "WeightedIndex", "FuturesPrice"], [1.0, 0.113329, 0.07388])),
+        dict(zip(["LEVERAGE", "WeightedIndex", "FuturesPrice"], [1.5, 0.176131, 0.100811])),
+        dict(zip(["LEVERAGE", "WeightedIndex", "FuturesPrice"], [2.0, 0.196055, 0.105226])),
+        dict(zip(["LEVERAGE", "WeightedIndex", "FuturesPrice"], [2.5, 0.234837, 0.092933])),
+        dict(zip(["LEVERAGE", "WeightedIndex", "FuturesPrice"], [3.0, 0.234157, 0.066772])),
+        dict(zip(["LEVERAGE", "WeightedIndex", "FuturesPrice"], [3.5, 0.242749, 0.028326])),
+        dict(zip(["LEVERAGE", "WeightedIndex", "FuturesPrice"], [4.0, 0.219047, -0.08158])),
+        dict(zip(["LEVERAGE", "WeightedIndex", "FuturesPrice"], [4.5, 0.195386, -0.09382])),
+        dict(zip(["LEVERAGE", "WeightedIndex", "FuturesPrice"], [5.0, 0.151051, -0.11831])),
+        dict(zip(["LEVERAGE", "WeightedIndex", "FuturesPrice"], [5.5, 0.095163, -0.12339])),
+        dict(zip(["LEVERAGE", "WeightedIndex", "FuturesPrice"], [6.0, 0.016063, -0.13232])),
+    ],  # with 60 data points as interval of moving average
+    "INTERVAL": [
+        dict(zip(["INTERVAL", "WeightedIndex", "FuturesPrice"], [10, 0.1049, 0.0375])),
+        dict(zip(["INTERVAL", "WeightedIndex", "FuturesPrice"], [20, 0.0823, 0.0565])),
+        dict(zip(["INTERVAL", "WeightedIndex", "FuturesPrice"], [30, 0.1247, 0.0751])),
+        dict(zip(["INTERVAL", "WeightedIndex", "FuturesPrice"], [40, 0.1365, 0.0943])),
+        dict(zip(["INTERVAL", "WeightedIndex", "FuturesPrice"], [50, 0.1285, 0.0798])),
+        dict(zip(["INTERVAL", "WeightedIndex", "FuturesPrice"], [60, 0.1133, 0.0739])),
+        dict(zip(["INTERVAL", "WeightedIndex", "FuturesPrice"], [70, 0.0963, 0.0594])),
+        dict(zip(["INTERVAL", "WeightedIndex", "FuturesPrice"], [80, 0.0999, 0.0787])),
+        dict(zip(["INTERVAL", "WeightedIndex", "FuturesPrice"], [90, 0.0938, 0.0666])),
+        dict(zip(["INTERVAL", "WeightedIndex", "FuturesPrice"], [100, 0.0955, 0.0581])),
+        dict(zip(["INTERVAL", "WeightedIndex", "FuturesPrice"], [110, 0.0815, 0.0549])),
+    ],  # with leverage = 2
+}
+
 
 
 @clock
-def estimate_effect(obj, leverages, intervals):
+def estimate_leverage_effect(obj, leverages, intervals):
     result = []
     for leverage in leverages:
         for interval in intervals:
@@ -31,22 +47,37 @@ def estimate_effect(obj, leverages, intervals):
             reserve = res_latest["Reserve"]
             yearly_rate_of_return = (reserve / res._INITIAL_RESERVE) ** (1 / 20) - 1
             result += [{"LEVERAGE": leverage,
-                        "YearlyROI": yearly_rate_of_return}]
+                        "WeightedIndex": yearly_rate_of_return}]
     return result
 
 
-def graph_effect(results, target):
-    leverages, book_sample, my_implement = [], [], []
+@clock
+def estimate_interval_effect(obj, leverages, intervals):
+    result = []
+    for leverage in leverages:
+        for interval in intervals:
+            res = obj.calculate(leverage, interval)
+            res_table = res.get()
+            res_latest = res_table.rows[-1]
+            reserve = res_latest["Reserve"]
+            yearly_rate_of_return = (reserve / res._INITIAL_RESERVE) ** (1 / 20) - 1
+            result += [{"INTERVAL": interval,
+                        "WeightedIndex": yearly_rate_of_return}]
+    return result
+
+
+def graph_effect(results, factor, target):
+    x, book_sample, my_implement = [], [], []
     for i in range(len(results)):
-        leverages += [leverage_matrix[i][target]]
-        book_sample += [leverage_matrix[i]["YearlyROI"] * 100]
-        my_implement += [results[i]["YearlyROI"] * 100]
+        x += [sample_matrix[factor][i][factor]]
+        book_sample += [sample_matrix[factor][i][target] * 100]
+        my_implement += [results[i][target] * 100]
 
     plt.figure()
-    plt.plot(leverages, book_sample, "o-")
-    plt.plot(leverages, my_implement, "o-")
+    plt.plot(x, book_sample, "o-")
+    plt.plot(x, my_implement, "o-")
     plt.ylabel("Yearly Rate Of Return, %")
-    plt.xlabel("Leverage")
+    plt.xlabel(factor.title())
     plt.legend(["book sample", "my implement"])
     plt.show()
 
@@ -56,7 +87,10 @@ vi_w = WeightedIndex(conf)
 vi_w.init()
 
 vi_f = FuturesPrice(conf)
-vi_f.init()
+# vi_f.init()
 
-results = estimate_effect(vi_w, [i * 0.5 for i in range(2, 13)], [60])
-graph_effect(results, "LEVERAGE")
+# results = estimate_leverage_effect(vi_w, [i * 0.5 for i in range(2, 13)], [60])
+# graph_effect(results, "LEVERAGE", "WeightedIndex")
+
+results = estimate_interval_effect(vi_w, [2], [i * 10 for i in range(1, 12)])
+graph_effect(results, "INTERVAL", "WeightedIndex")
