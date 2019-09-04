c0abd5043442d1d0378be1b78f9bd0902198cb9a
Berchem <34000467+Berchem@users.noreply.github.com>
2019-09-04 19:56:18 +0800

    [UPDATE] HModel: _VolumeIndicator: calculate

--- test/VolumeTest.py
+++ test/VolumeTest.py
@@ -6,13 +6,9 @@ from Futures.Util import *
 from HModel.Volume import *
 import os
 import datetime as dt
-import pandas as pd
 
-conf = Config("test-conf.ini")
-
-BASE_DIR = conf.prop.get("VOLUME", "BASE_DIR")
-RESOURCE_DIR = conf.prop.get("VOLUME", "RESOURCE_DIR")
-filename = os.path.join(BASE_DIR, RESOURCE_DIR, "history_data_for_h_model.csv")
+conf = Config("../test_resources/conf/test-conf.ini")
+filename = conf.prop.get("VOLUME", "RESOURCE_FILENAME")
 print(type(conf.prop.get("VOLUME", "INTERVAL")))
 data_util = DataUtil()
 data = data_util.get_data_from_file(filename, 1)
@@ -42,27 +38,40 @@ print("isClose in tw:", ClosingDates(dt.date(1998, 9, 16), "fri", 3).is_closing(
 ma_index = MovingAverage(dt.datetime(1998, 9, 1), dt.timedelta(days=1), 60)
 
 
-def _calc(row):
-    time = dt.datetime.strptime(row["Date"], "%Y/%m/%d")
-    price = float(row["WeightedIndex"])
-    ma_index.update(time, price, price)
-    _, ma_value = ma_index.get("price")
-    return ma_value
-
-
-data_1 = data.select(additional_columns={"avg_index": _calc})
-data_1.rows = data_1.rows[::-1]
-
-print(data_1.limit(5))
-
 vi = WeightedIndex(conf)
-vi._load_data()
-vi._calc_ma()
-print(vi._data)
 
+vi.init()
+# print(sum(row["VolumeIndicator"] for row in vi.get().rows[59:]))
+# print("\n".join(map(str, vi.get().rows[59:85])))
+res = vi.calculate()
+print("vi data shape = (%d, %d)" % (len(vi._data.columns), len(vi._data.rows)), vi._data.columns)
+print("vi tmp shape = (%d, %d)" % (len(vi._tmp.columns), len(vi._tmp.rows)), vi._tmp.columns)
 
-excel_file = os.path.join(BASE_DIR, RESOURCE_DIR, "history_data_for_h_model.xlsx")
-excel_obj = pd.ExcelFile(excel_file)
-main_sheet = pd.read_excel(excel_obj, sheet_name=0)
+print("res data shape = (%d, %d)" % (len(res._data.columns), len(res._data.rows)), res._data.columns)
+print("res tmp shape = (%d, %d)" % (len(res._tmp.columns), len(res._tmp.rows)), res._tmp.columns)
+
+print("\n\n\n")
+
+import time
+
+results = []
+t0 = time.perf_counter()
+for i in range(8):
+    for j in range(10, 101, 10):
+        res = vi.calculate(i+1, j)
+        res_table = res.get()
+        res_latest = res_table.rows[-1]
+        reserve = res_latest["Reserve"]
+        expectation = (reserve / res._INITIAL_RESERVE) ** (1 / 20)
+        results += [(expectation, i, j)]
+
+print(time.perf_counter() - t0)
+
+high = 0
+for res in results:
+    print(res)
+    if res[0] > high:
+        high = res[0]
+        best = res
 print()
-
+print(best)
